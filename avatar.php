4fb5800fcb94aff2c2d135e364afac539470437a
Robert Farber <elminero@cox.net>
2016-09-23 18:46:47 -0700

    avatar composer

--- avatar.php
+++ avatar.php
@@ -1,7 +1,10 @@
 
 <!-- div 1 Start Avatar -->
     <div style="width: 175px; padding-bottom: 20px">
-    <a href="picture.php?id=<?php echo $contact->avatar->id; ?>"><img alt="" src="images/<?php echo $contact->avatar->path_file; ?>_t.jpg" /></a>
+
+    <?php if($contact->avatar): ?>
+        <a href="picture.php?id=<?php echo $contact->avatar->id; ?>"><img alt="" src="images/<?php echo $contact->avatar->path_file; ?>_t.jpg" /></a>
+    <?php endif ?>
     <br />
     <div style="float: left">
         <a href="addphotos.php?id=<?php echo $id ?>">View All</a>
